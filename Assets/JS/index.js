cf1e482021f934a8b0aaab17e10d727426246156
joe tech <ojtech2003@gmail.com>
2022-08-06 22:39:47 +0100

    faq

---
+++ Assets/JS/index.js
@@ -0,0 +1,16 @@
+const questions = document.querySelectorAll(".question-content");
+
+questions.forEach(function (question) {
+// console.log(question);
+const btn = question.querySelector(".question-btn");
+// console.log(btn);
+btn.addEventListener("click", function () {
+
+    questions.forEach(function (item) {
+        if (item !== question) {
+          item.classList.remove("show-text");
+        }
+      });
+        question.classList.toggle("show-text");
+});
+});
