0eb9f1d4c2cb8478938573f4a0309813aaf8e60e
joe tech <ojtech2003@gmail.com>
2022-08-09 07:13:25 +0100

    modal

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -14,3 +14,34 @@ btn.addEventListener("click", function () {
         question.classList.toggle("show-text");
 });
 });
+
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtns = document.querySelectorAll(".modal-btnx")
+const modal = document.querySelector(".modal-overlay");
+const modalX = document.querySelector(".overlayx")
+const closeBtn = document.querySelector(".close-btn");
+const closeBtnX = document.querySelector(".close-btnx");
+
+modalBtn.forEach(function (mode) {
+  
+mode.addEventListener("click", function () {
+    modal.classList.add("open-modal");
+});
+closeBtn.addEventListener("click", function(){
+    modal.classList.remove("open-modal");
+});
+})
+modalBtns.forEach(function (btn) {
+  
+btn.addEventListener("click", function () {
+    modalX.classList.add("open-modal");
+});
+closeBtnX.addEventListener("click", function(){
+    modalX.classList.remove("open-modal");
+});
+})
